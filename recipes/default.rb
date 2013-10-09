6b913e84d2af779bb193dcc3846741447f1e5720
Steven Dunlap <steven@roadtrippers.com>
2013-10-09 18:04:57 -0400

    create default recipe [#58518770]

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,17 @@
 # Cookbook Name:: mosql
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Roadtrippers 
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under MIT; see LICENSE 
 #
+
+gem_package 'mosql'
+
+Chef::Application.fatal!('node[\'mosql\'][\'collections_path\'] much be set') if node['mosql']['collections_path'].nil?
+collections_path_flag = "-c #{node['mosql']['collections_path']}"
+mosql_flag = node['mosql']['mongo'].nil? ? '' : "--mongo #{node['mosql']['mongo']}"
+sql_flag = node['mosql']['sql'].nil? ? '' : "--sql #{node['mosql']['sql']}"
+bash 'mosql' do
+  code "mosql #{collections_path_flag} #{mosql_flag} #{sql_flag}"
+end
