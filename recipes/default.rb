42712491c5cc850d76e7b41aad6d8e3fb2246980
Steven Dunlap <steven@roadtrippers.com>
2013-10-12 16:12:31 -0400

    WIP work on installing mosql from remote branch

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,21 @@
 # Licensed under MIT; see LICENSE 
 #
 
-gem_package 'mosql'
-
-Chef::Application.fatal!('node[\'mosql\'][\'collections_path\'] much be set') if node['mosql']['collections_path'].nil?
-collections_path_flag = "-c #{node['mosql']['collections_path']}"
-mosql_flag = node['mosql']['mongo'].nil? ? '' : "--mongo #{node['mosql']['mongo']}"
-sql_flag = node['mosql']['sql'].nil? ? '' : "--sql #{node['mosql']['sql']}"
-bash 'mosql' do
-  code "mosql #{collections_path_flag} #{mosql_flag} #{sql_flag}"
+# temporarily install gem from rt fork
+git '/usr/local/src/mosql' do
+  repository 'git@github.com:roadtrippers/mosql.git'
 end
+
+execute 'gem build mosql.gemspec' do
+  cdw '/usr/local/src/mosql'
+end
+
+# gem_package 'mosql'
+# 
+# Chef::Application.fatal!('node[\'mosql\'][\'collections_path\'] much be set') if node['mosql']['collections_path'].nil?
+# collections_path_flag = "-c #{node['mosql']['collections_path']}"
+# mosql_flag = node['mosql']['mongo'].nil? ? '' : "--mongo #{node['mosql']['mongo']}"
+# sql_flag = node['mosql']['sql'].nil? ? '' : "--sql #{node['mosql']['sql']}"
+# bash 'mosql' do
+#   code "mosql #{collections_path_flag} #{mosql_flag} #{sql_flag}"
+# end
